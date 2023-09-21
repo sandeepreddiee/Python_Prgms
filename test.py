cdc33aad19cff283e68edcf0fb757dd5697022b7
Sandeep Reddy <sandeep.gandluri@gmail.com>
2023-09-21 16:12:34 -0500

    First Commit

---
+++ test.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('Data-Week22.csv')
+print(df)
---
+++ test_my_script.py
@@ -0,0 +1,7 @@
+from my_script import add, subtract
+
+def test_add():
+    assert add(1, 2) == 3
+
+def test_subtract():
+    assert subtract(5, 2) == 3
